c0744358b635d66037647c707a709a2321ef330d
mr-ibrahim-faiz <mr.ibrahim.faiz@gmail.com>
2019-05-09 15:34:46 +0200

    Minor changes.

--- adb_helper/backup.cpp
+++ adb_helper/backup.cpp
@@ -6,4 +6,5 @@ void backup_directory(const string& directory){
 	const string command = "adb pull " + directory + " " + backup_root_directory; 
 	run_command(command);
 	remove_directory(directory);
+	open_directory(backup_root_directory);
 }
